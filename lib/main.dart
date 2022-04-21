af64c8422e436605dd9b5bb457a6139b36324c8e
Cabral221 <cabraldiop18@gmail.com>
2022-04-21 12:55:38 +0000

    Feat : Initial Material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const appTitle = 'République du Sénégal';
+
   @override
   Widget build(BuildContext context) {
-    const firstColor = Color(0xFF2A53C1);
+    const Map<int, Color> color = {
+      50: Color.fromRGBO(2, 42, 105, .1),
+      100: Color.fromRGBO(2, 42, 105, .2),
+      200: Color.fromRGBO(2, 42, 105, .3),
+      300: Color.fromRGBO(2, 42, 105, .4),
+      400: Color.fromRGBO(2, 42, 105, .5),
+      500: Color.fromRGBO(2, 42, 105, .6),
+      600: Color.fromRGBO(2, 42, 105, .7),
+      700: Color.fromRGBO(2, 42, 105, .8),
+      800: Color.fromRGBO(2, 42, 105, .9),
+      900: Color.fromRGBO(2, 42, 105, 1),
+    };
 
     return MaterialApp(
-      title: "Ministre du Dévelmoppement communautaire, de l’Equité sociale et territoriale",
+      title: appTitle,
+      home: const MyHomePage(title: appTitle),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        primaryColor: firstColor,
+        primarySwatch: const MaterialColor(0xFF2A53C1, color),
+        textTheme: TextTheme(
+          headline1: GoogleFonts.merriweather(
+              fontSize: 92, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+          headline2: GoogleFonts.merriweather(
+              fontSize: 57, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+          headline3: GoogleFonts.merriweather(
+              fontSize: 46, fontWeight: FontWeight.w400),
+          headline4: GoogleFonts.merriweather(
+              fontSize: 32, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+          headline5: GoogleFonts.merriweather(
+              fontSize: 23, fontWeight: FontWeight.w400),
+          headline6: GoogleFonts.merriweather(
+              fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+          subtitle1: GoogleFonts.merriweather(
+              fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+          subtitle2: GoogleFonts.merriweather(
+              fontSize: 13, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+          bodyText1: GoogleFonts.openSans(
+              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+          bodyText2: GoogleFonts.openSans(
+              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+          button: GoogleFonts.openSans(
+              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+          caption: GoogleFonts.openSans(
+              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+          overline: GoogleFonts.openSans(
+              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+        ),
       ),
-      home: const MyHomePage(title: "MDCEST"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have MDCEST the button this many times:',
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/logo-ministere.jpg'),
+            Container(
+              padding: EdgeInsets.zero,
+              child: const Center(child: Text('Salut')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ]),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 2, 42, 105),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(4.0),
+                margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Ministére du développement communautaire, de l\'Équité Sociale et Territoriale',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 18),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: const Text(
+                        'Menu',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Le Ministére'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Actualités'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Programmes'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
